9bfbc979856e311696ed8031fe74ca0a045ea96a
artemiypyki <kokoartem0707@mail.ru>
2026-06-19 20:14:01 +0700

    готово

--- app/src/androidTest/java/com/kokoshnikov/pr21_kokoshnikov/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kokoshnikov/pr21_kokoshnikov/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.abdulaziz.pr21_mirzakamilov_pr23103;
+package com.abdulaziz.pr21_kokoshnikov;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.abdulaziz.pr21_mirzakamilov_pr23103", appContext.getPackageName());
+        assertEquals("com.abdulaziz.pr21_kokoshnikov", appContext.getPackageName());
     }
 }
